5436b9c5def438a602e2d956546bfcce8f9bfb0a
Suhaib El-komy <44414827+SuhaibElkomy@users.noreply.github.com>
2019-03-31 21:16:52 -0400

    Update Qoute.php

--- _includes/Qoute.php
+++ _includes/Qoute.php
@@ -1,1 +1,23 @@
+<?php
+
+  $fname = $_POST['name'];
+
+  $lname_email = $_POST['email'];
+
+  $country = $_POST['message'];
+
+$email_from = 'Suhaib@613iphonerepair.ca';
+
+ 
+
+    $email_subject = "New Form submission";
+
+ 
+
+    $email_body = "You have received a new message from the user $fname. $country \n".
+
+                            "Here is the message:\n $message".
+
+
+?>
 
